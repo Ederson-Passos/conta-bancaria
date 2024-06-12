f4c595875af88c6739bb44737f31945b742e4e8c
Ederson Passos <145729066+Ederson-Passos@users.noreply.github.com>
2024-06-12 10:22:40 -0300

    Add files via upload

---
+++ Account.java
@@ -0,0 +1,12 @@
+/*Uma classe que representa uma conta bancária com seus métodos.*/
+public class Account {
+    private String name;
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
